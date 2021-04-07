417ca37712feb87dc4359ea9e556251ee6c62ffd
Michael Platzer <michael.platzer@hotmail.com>
2021-04-07 22:17:49 +0200

    Plot altitude profile for path

--- find_path.py
+++ find_path.py
@@ -54,8 +54,10 @@ print("Searching path ...")
 
 #coord_start = (1586302.74, 6202211.87)
 #coord_goal  = (1783338.24, 5915996.99)
-coord_start = (1373668.944570, 6052995.719446)
-coord_goal  = (1526447.454855, 5873019.959034)
+#coord_start = (1373668.944570, 6052995.719446)
+#coord_goal  = (1526447.454855, 5873019.959034)
+coord_start = (1615267.281722, 6207771.147095)
+coord_goal  = (1862674.833024, 6223846.809606)
 
 # convert start and goal coordinates to nodes
 grid_start = (
@@ -100,3 +102,71 @@ draw.save(f"path_{GRID_RESOLUTION}x{GRID_RESOLUTION}.png")
 #    total_delta += diff
 #
 #print(f"Total delta: {total_delta}")
+
+###############################################################################
+# generate altitude profile
+
+path_len = GRID_RESOLUTION * sum(
+    math.sqrt((x1 - x2)**2 + (y1 - y2)**2) for (x1, y1), (x2, y2)
+                                           in zip(path, path[1:])
+)
+delta_alt = 0.
+min_slope, max_slope = 0., 0.
+
+svg_scale    = (0.1, 1.)
+profile_size = (int(path_len * svg_scale[0]), int(4000. * svg_scale[1]))
+
+with open(f"profile_{GRID_RESOLUTION}x{GRID_RESOLUTION}.svg", 'w') as svg:
+    svg.write('<svg ')
+    svg.write(f"width=\"{profile_size[0]}\" height=\"{profile_size[1]}\" ")
+    svg.write(f"viewBox=\"0 0 {profile_size[0]} {profile_size[1]}\" ")
+    svg.write('xmlns=\"http://www.w3.org/2000/svg\">\n')
+
+    terrain_line = [(0., grid.get_node_value(path[0]))]
+    xpos = 0.
+    for (x1, y1), (x2, y2) in zip(path, path[1:]):
+        dist  = GRID_RESOLUTION * math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+        alt1  = grid.get_node_value((x1, y1))
+        alt2  = grid.get_node_value((x2, y2))
+        slope = (alt2 - alt1) / dist
+        # stats
+        delta_alt += abs(alt1 - alt2)
+        min_slope  = min(min_slope, slope)
+        max_slope  = max(max_slope, slope)
+        # svg output
+        stops = [
+            (255, 255, 255), ((255, 0, 0) if slope >= 0. else (0, 0, 255))
+        ]
+        color = tuple(
+            int((1. - abs(slope)) * stop1 + abs(slope) * stop2)
+            for stop1, stop2 in zip(stops[0], stops[1])
+        )
+        alt1, alt2 = alt1 * svg_scale[1], alt2 * svg_scale[1]
+        svg.write(f"  <polygon points=\"")
+        svg.write(f"{xpos                      },{profile_size[1]       } ")
+        svg.write(f"{xpos + dist * svg_scale[0]},{profile_size[1]       } ")
+        svg.write(f"{xpos + dist * svg_scale[0]},{profile_size[1] - alt2} ")
+        svg.write(f"{xpos                      },{profile_size[1] - alt1}" )
+        svg.write(f"\" fill=\"rgb{color}\" stroke=\"none\" />\n")
+        xpos += dist * svg_scale[0]
+        terrain_line.append((xpos, alt2))
+
+    svg.write('  <polyline points=\"')
+    svg.write(' '.join(f"{x},{profile_size[1] - y}" for x, y in terrain_line))
+    svg.write(f"\" fill=\"none\" stroke=\"black\" />\n")
+
+    svg.write('</svg>')
+
+print(f"  path length:      {path_len / 1000.:.3f} km")
+print(f"  steepest climb:   {max_slope * 100.:.2f} %")
+print(f"  steepest descent: {min_slope * 100.:.2f} %")
+
+#<svg width="391" height="391" viewBox="-70.5 -70.5 391 391" xmlns="http://www.w3.org/2000/svg">
+#<rect fill="#fff" stroke="#000" x="-70" y="-70" width="390" height="390"/>
+#<g opacity="0.8">
+#<rect x="25" y="25" width="200" height="200" fill="lime" stroke-width="4" stroke="pink" />
+#   <circle cx="125" cy="125" r="75" fill="orange" />
+#   <polyline points="50,150 50,200 200,200 200,100" stroke="red" stroke-width="4" fill="none" />
+#   <line x1="50" y1="50" x2="200" y2="200" stroke="blue" stroke-width="4" />
+#</g>
+#</svg>
